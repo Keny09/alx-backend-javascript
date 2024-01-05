1359cbbe66f7dd999b2637038ee2f53d57f21a70
Keny09 <rosekendy09@gmail.com>
2024-01-05 21:45:06 +0000

    ES6 Basics

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,16 +1,20 @@
-// 0-constants.js
-export function taskFirst() {
+function taskFirst() {
  const task = 'I prefer const when I can.';
  return task;
 }
 
-export function getLast() {
- return ' is okay';
-}
-
-export function taskNext() {
+function taskNext() {
  let combination = 'But sometimes let';
  combination += getLast();
 
  return combination;
 }
+
+function getLast() {
+ return ' is okay';
+}
+
+module.exports = {
+ taskFirst,
+ taskNext,
+};
